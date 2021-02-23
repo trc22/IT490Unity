018bcd9015a95e91edb830767b62be723411266f
Tim <trc22@njit.edu>
2021-02-23 12:27:57 -0500

    Setting up project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,62 +1,18 @@
-﻿using Mirror;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : NetworkBehaviour
+public class Player : MonoBehaviour
 {
-    [SyncVar(hook = nameof(OnHolaCountChanged))]
-    int holaCount = 0;
-
-    void HandleMovement()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (isLocalPlayer)
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
-            transform.position = transform.position + movement;
-        }
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        HandleMovement();
-
-        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
-        {
-            Debug.Log("Sending Hola to Server!");
-            Hola();
-        }
-    }
-
-    public override void OnStartServer()
-    {
-        Debug.Log("Player has been spawned on the server!");
-    }
-
-    [Command]
-    void Hola()
-    {
-        Debug.Log("Received Hola from Client!");
-        holaCount += 1;
-        ReplyHola();
-    }
-
-    [TargetRpc]
-    void ReplyHola()
-    {
-        Debug.Log("Received Hola from Server!");
-    }
-
-    [ClientRpc]
-    void TooHigh()
-    {
-        Debug.Log("Too high!");
-    }
-
-    void OnHolaCountChanged(int oldCount, int newCount)
-    {
-        Debug.Log($"We had {oldCount} holas, but now we have {newCount} holas!");
+        
     }
 }
