b6197b98f860b604999dd8beaad32e58984522bf
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-03-02 09:37:26 -0500

    Added more Effects for paddle and Ball, and edited their interactions

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 public class Ball : NetworkBehaviour
 {
     public Rigidbody2D ball_rigidBody;
+    public Animator WeatherStage;
+    public SpriteRenderer mySprite;
     enum Weather
     {
         REGULAR, RAIN, HOT, SNOW, HAIL, WIND, THUNDER
     }
-    Weather currentWeather = Weather.REGULAR;
+    [SerializeField] Weather currentWeather = Weather.REGULAR;
+
     public override void OnStartServer()
     {
         base.OnStartServer();
-
+        
         // only simulate ball physics on server
         ball_rigidBody.simulated = true;
 
@@ -25,10 +28,70 @@ public class Ball : NetworkBehaviour
     {
         Debug.Log("Collision!");
         //Paddle physics
+        if (mySprite.flipX != true)
+        {
+            mySprite.flipX = true;
+        }
+        else
+        {
+            mySprite.flipX = false;
+        }
+        
+        WeatherEffect(col);
+    }
+    private void Update()
+    {
+        WeatherStage.SetInteger("Weather_Ball", (int)currentWeather);  
+    }
+
+    void WeatherEffect(Collision2D col)
+    {
+        
+        //regular speed
+        if (currentWeather == Weather.REGULAR)
+        {
+            SpeedAndWeather(1f,col);
+        }
+        //super speed
+        else if (currentWeather == Weather.HOT)
+        {
+            SpeedAndWeather(2f, col);
+        }
+        //speed decrease/ slip
+        else if (currentWeather == Weather.RAIN)
+        {
+            SpeedAndWeather(0.5f, col);
+        }
+        //move on steps
+        else if (currentWeather == Weather.SNOW)
+        {
+            SpeedAndWeather(1f, col);
+        }
+
+        //hit by hail/ pushed down from time to time
+        else if (currentWeather == Weather.HAIL)
+        {
+            SpeedAndWeather(1f, col);
+        }
+        //move fast
+        else if (currentWeather == Weather.WIND)
+        {
+            SpeedAndWeather(1f, col);
+        }
+        //move fast on bursts
+        else if (currentWeather == Weather.THUNDER)
+        {
+            SpeedAndWeather(1f, col);
+        }
+    }
+
+    void SpeedAndWeather(float speed, Collision2D col)
+    {
+        
         if (col.collider.offset.y == 0.32f) //if hit top collider
-            ball_rigidBody.velocity += (Vector2.up);
+            ball_rigidBody.velocity += (Vector2.up)*speed;
         else if (col.collider.offset.y == -0.32f) //if hit bottom collider
-            ball_rigidBody.velocity += (Vector2.down);
+            ball_rigidBody.velocity += (Vector2.down)*speed;
     }
 
 }
