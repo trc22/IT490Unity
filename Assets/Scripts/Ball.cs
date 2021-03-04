34879e28667b1f8964c66853a8258fa8eb01463f
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-03-04 11:25:08 -0500

    Completed Behaviors, this is a commit before troubleshooting them

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,6 +6,9 @@ public class Ball : NetworkBehaviour
     public Rigidbody2D ball_rigidBody;
     public Animator WeatherStage;
     public SpriteRenderer mySprite;
+    private bool change;
+    
+    private int delay =100;
     enum Weather
     {
         REGULAR, RAIN, HOT, SNOW, HAIL, WIND, THUNDER
@@ -46,7 +49,7 @@ public class Ball : NetworkBehaviour
 
     void WeatherEffect(Collision2D col)
     {
-        
+        Debug.Log("Speed: "+ ball_rigidBody.velocity);
         //regular speed
         if (currentWeather == Weather.REGULAR)
         {
@@ -56,41 +59,85 @@ public class Ball : NetworkBehaviour
         else if (currentWeather == Weather.HOT)
         {
             SpeedAndWeather(2f, col);
+               
+            
         }
         //speed decrease/ slip
         else if (currentWeather == Weather.RAIN)
         {
             SpeedAndWeather(0.5f, col);
+
         }
         //move on steps
         else if (currentWeather == Weather.SNOW)
         {
-            SpeedAndWeather(1f, col);
+            if (delay != 100)
+            {
+                delay += 10;
+                if (delay < 50)
+                {
+                    SpeedAndWeather(1f, col);
+                }
+            }
+            else delay = 0;
         }
 
         //hit by hail/ pushed down from time to time
         else if (currentWeather == Weather.HAIL)
         {
-            SpeedAndWeather(1f, col);
+           
         }
         //move fast
         else if (currentWeather == Weather.WIND)
         {
             SpeedAndWeather(1f, col);
+            if (delay < 200)
+            {
+                if (delay > 100)
+                {
+                    SpeedAndWeather(2f, col);
+                }
+                else
+                {
+                    SpeedAndWeather(0.5f, col);
+                }
+                delay += 5;
+            }
+            else delay = 0;
+
         }
         //move fast on bursts
         else if (currentWeather == Weather.THUNDER)
         {
-            SpeedAndWeather(1f, col);
+            if (delay <200)
+            {
+                delay += 5;
+                if (delay > 60)
+                {
+                    SpeedAndWeather(1f, col);
+                }
+                if (delay == 200) delay = 0;
+
+            }
+            
         }
     }
 
     void SpeedAndWeather(float speed, Collision2D col)
     {
         
-        if (col.collider.offset.y == 0.32f) //if hit top collider
+        if (col.collider.offset.y == 0.08f) //if hit top collider
             ball_rigidBody.velocity += (Vector2.up)*speed;
-        else if (col.collider.offset.y == -0.32f) //if hit bottom collider
+        if (col.collider.offset.y == 0.0f)
+        {
+            if (change)
+            {
+                ball_rigidBody.velocity *= speed;
+            }
+            
+
+        }
+        else if (col.collider.offset.y == -0.08f) //if hit bottom collider
             ball_rigidBody.velocity += (Vector2.down)*speed;
     }
 
