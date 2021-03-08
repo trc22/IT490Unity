ae9fdbe7d60ed92b7719e9da4d4497df6549e118
Tim <trc22@njit.edu>
2021-03-07 21:23:22 -0500

    Webserver communicates with game

---
+++ Assets/Scripts/Web.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+// UnityWebRequest.Get example
+
+// Access a website and use UnityWebRequest.Get to download a page.
+// Also try to download a non-existing page. Display the error.
+
+public class Web : MonoBehaviour
+{
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("http://192.168.1.62/"))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
