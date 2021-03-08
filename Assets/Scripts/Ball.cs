95c5d5dc3bd9d714572e136eebc6b659c3ef0ed6
Tim <trc22@njit.edu>
2021-03-07 21:56:28 -0500

    Manually merged master and Animation branch (data loss occurs when merge conflicts happen with .prefab files)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,10 +1,30 @@
 ﻿using Mirror;
 using UnityEngine;
+using System.Collections;
 
 public class Ball : NetworkBehaviour
 {
     public Rigidbody2D ball_rigidBody;
     public GameObject gameManager;
+
+    public Animator WeatherStage;
+    
+    public SpriteRenderer mySprite;
+    private bool change = false; //this change is used for all behaviors that increase or decrease things, is used so they dont infinetly do that.
+    private bool check = false; //this check is used for thunder behavio(as it works differently it needs its own in tandom with the 'change',
+                                // as its to check that it does not infinetly increase its speed.
+    
+    
+    enum Weather
+    {
+        REGULAR, RAIN, HOT, SNOW, HAIL, WIND, THUNDER
+    }
+    [SerializeField] Weather currentWeather = Weather.REGULAR;
+    enum Stages
+    {
+        OFF, NORMAL, FAST
+    }
+    Stages currentStage = Stages.OFF;
 
     public override void OnStartServer()
     {
@@ -25,16 +45,25 @@ public class Ball : NetworkBehaviour
     {
         Debug.Log("Collision!");
         //Paddle physics
-        if (col.collider.offset.y == 0.32f) //if hit top collider
-            ball_rigidBody.velocity += (Vector2.up / 2f);
-        else if (col.collider.offset.y == -0.32f) //if hit bottom collider
-            ball_rigidBody.velocity += (Vector2.down / 2f);
+
+        //Flips the sprite only when hiting the paddles
+        if(col.collider.offset.y != -2.88f || col.collider.offset.y != -2.88f)
+        {
+            if (mySprite.flipX != true) mySprite.flipX = true;
+
+            else mySprite.flipX = false;
+        }
+        
+        //Function that handles weather effects
+        WeatherEffect(col);
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if(gameManager == null)
                 gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        
+         WeatherStage.SetInteger("Weather_Ball", (int)currentWeather);  
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -69,5 +98,165 @@ public class Ball : NetworkBehaviour
             ball_rigidBody.velocity = Vector2.left * 3f;
     }
 
+    IEnumerator Wait(Collision2D col,float start, float end, float secs)
+    {
+        change = false;
+        if(check == false && currentWeather == Weather.THUNDER)
+        {
+            SpeedAndWeather(1f, col);
+            check = true;
+        }
+        else
+        {
+            SpeedAndWeather(start, col);
+        }
+        yield return new WaitForSeconds(secs);
+        change = false;
+        SpeedAndWeather(end, col);
+        
+        
+    }
+
+    void WeatherEffect(Collision2D col)
+    {
+        Debug.Log("Speed: "+ ball_rigidBody.velocity);
+        //regular speed
+        if (currentWeather == Weather.REGULAR)
+        {
+            SpeedAndWeather(1f,col);
+        }
+        //super speed
+        else if (currentWeather == Weather.HOT)
+        {
+            SpeedAndWeather(3f, col);
+               
+            
+        }
+        //speed decrease/ slip
+        else if (currentWeather == Weather.RAIN)
+        {
+            SpeedAndWeather(0.75f, col);
+
+        }
+        //move on steps
+        else if (currentWeather == Weather.SNOW)
+        {
+
+            SpeedAndWeather(0.50f, col);
+
+        }
+
+        //hit by hail/ pushed down from time to time
+        else if (currentWeather == Weather.HAIL)
+        {
+           
+           if(currentStage == Stages.FAST || currentStage == Stages.OFF)
+            {
+                SpeedAndWeather(2f, col);
+                currentStage = Stages.NORMAL;
+            }
+            else
+            {
+                SpeedAndWeather(.5f, col);
+                currentStage = Stages.FAST;
+            }
+        }
+        //move fast
+        else if (currentWeather == Weather.WIND)
+        {
+            StartCoroutine(Wait(col,4f,.25f,2f));
+
+        }
+        //move fast on bursts
+        else if (currentWeather == Weather.THUNDER)
+        {
+            StartCoroutine(Wait(col, .5f, 2f,1.2f));
+
+        }
+    }
+
+    void SpeedAndWeather(float speed, Collision2D col)
+    {
+        //if hit top collider
+        if (currentStage != Stages.OFF)
+        {
+            change = false;
+        }
+        if (col.collider.offset.y == 0.08f)
+        {
+            
+            if (!change)
+            {
+                ball_rigidBody.velocity += (Vector2.up) * speed;
+                change = true;
+            }
+            else
+            {
+                ball_rigidBody.velocity += (Vector2.up);
+            }
+
+
+        }
+        //if hit middle collider
+
+        else if (col.collider.offset.y == 0.0f)
+        {
+            if (!change)
+            {
+                ball_rigidBody.velocity *= speed;
+                change = true;
+            }
+            else
+            {
+                ball_rigidBody.velocity += Vector2.right;
+            }
+        }
+
+        //if hit bottom collider
+
+        else if (col.collider.offset.y == -0.08f) {
+            if (!change)
+            {
+                ball_rigidBody.velocity += (Vector2.down) * speed;
+                change = true;
+            }
+            else
+            {
+                ball_rigidBody.velocity += (Vector2.down);
+            }
+        }
+
+        //hits top Boundary
+
+        else if (col.collider.offset.y == -2.88f)
+        {
+            if (!change)
+            {
+                ball_rigidBody.velocity += (Vector2.down) * speed;
+                change = true;
+            }
+            else
+            {
+                ball_rigidBody.velocity += (Vector2.down);
+            }
+        }
+        
+
+        //hits bottom Boundary
+
+        else if (col.collider.offset.y == 2.88f)
+        {
+            if (!change)
+            {
+                ball_rigidBody.velocity += (Vector2.up) * speed;
+                change = true;
+            }
+            else
+            {
+                ball_rigidBody.velocity += (Vector2.up);
+            }
+        }
+        
+    }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,152 @@
 ﻿using Mirror;
 using UnityEngine;
+using System.Collections;
 
 public class Player : NetworkBehaviour
 {
     public Transform playerTransform;
     float accel = 0.8f;
+
+    public Animator WeatherStage; //Handles the Animations
+    int delay; //handles delays for effects
+    [SerializeField]int threshold = 200; //Handles the max amount of Delay
+    enum Weather
+    {
+        REGULAR, RAIN, HOT, SNOW, HAIL, WIND, THUNDER
+    }
+    [SerializeField]Weather currentWeather = Weather.REGULAR;
+
+
     void Movement()
     {
-        if(isLocalPlayer)
-        {   
-            if (Input.GetKey("up"))
-            {
-                if (playerTransform.position.y < 7.2f) //Upper limit
-                    playerTransform.position += Vector3.up * accel;
-            }
-            if (Input.GetKey("down"))
-            {
-                if (playerTransform.position.y > -4.85f) //Lower limit
-                    playerTransform.position += Vector3.down * accel;
-            }
+        
+        
+        if (isLocalPlayer)
+        {
+            //Handles Weather effects
+            WeatherEffect();
+            
         }
     }
 
+
+    void WeatherEffect()
+    {
+        WeatherStage.SetInteger("Weather", (int) currentWeather);
+        //regular speed
+        if (currentWeather == Weather.REGULAR)
+        {
+            SpeedAndWeather(accel);
+        }
+        //super speed
+        else if (currentWeather == Weather.HOT)
+        {
+            SpeedAndWeather(accel * 4);
+        }
+        //speed decrease/ slip
+        else if (currentWeather == Weather.RAIN)
+        {
+            SpeedAndWeather(accel / 4);
+        }
+        //move on steps
+        else if (currentWeather == Weather.SNOW)
+        {
+            if (delay != 100)
+            {
+                delay += 10;
+                if (delay < 50)
+                {
+                    SpeedAndWeather(accel);
+                }
+            }
+            else delay = 0;
+            
+            
+        }
+
+        //hit by hail/ pushed down from time to time
+        else if (currentWeather == Weather.HAIL)
+        {
+            SpeedAndWeather(accel);
+        }
+        //move fast at randomtimes due to wind, but then slow back down
+        else if (currentWeather == Weather.WIND)
+        {
+            Debug.Log(delay);
+            if (delay < 200){
+                if (delay > 100)
+                {
+                    SpeedAndWeather(accel*3);
+                }
+                else
+                {
+                    SpeedAndWeather(accel/2);
+                }
+                delay += 5;
+            }
+            else  delay = 0;
+
+
+            
+        }
+        //move fast on bursts then studder due to the lightning
+        else if (currentWeather == Weather.THUNDER)
+        {
+            
+            Speed_DelayAndWeather(accel*3);
+
+        }
+    }
+
+    void SpeedAndWeather(float accel)
+    { 
+        
+        if (Input.GetKey("up"))
+        {
+            if (playerTransform.position.y < 7f) //Upper limit
+                playerTransform.position += Vector3.up * accel;
+        }
+        if (Input.GetKey("down"))
+        {
+            if (playerTransform.position.y > -4.5f) //Lower limit
+                playerTransform.position += Vector3.down * accel;
+        }
+    }
+
+    void Speed_DelayAndWeather(float accel)
+    {
+
+        if (Input.GetKey("up"))
+        {
+            if (delay < threshold)
+            {
+                delay += 5;
+                if (delay > 60)
+                {
+                    if (playerTransform.position.y < 7f) //Upper limit
+                        playerTransform.position += Vector3.up * accel;
+                }
+                if (delay == threshold) delay = 0;
+
+            }
+        }
+        if (Input.GetKey("down"))
+        {
+            if (delay < threshold)
+            {
+                delay += 5;
+                if (delay > 60)
+                {
+                    if (playerTransform.position.y > -4.5f) //Lower limit
+                    playerTransform.position += Vector3.down * accel;
+            }
+            if (delay == threshold) delay = 0;
+
+            }
+        }
+    }
     // Update is called once per frame
+
     void FixedUpdate()
     {
         Movement();
