f0f88990754485d9650c9b396b5afcfb85a8f329
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-03-09 12:55:39 -0500

    added Historical Settings toggle

---
+++ Assets/Scripts/Historical_Toggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Historical_Toggle : MonoBehaviour
+{
+    public GameObject inputField;
+    public Toggle historicSettingToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField.SetActive(false);
+    }
+
+
+    public void Update()
+    {
+        if (historicSettingToggle.isOn)
+        {
+            inputField.SetActive(true);
+        }
+        else
+        {
+            inputField.SetActive(false);
+        }
+    }
+}
